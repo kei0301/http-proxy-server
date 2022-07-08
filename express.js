f47fa93edad561b0d07e36fef7455d40fd78ccf9
superstar323 <carlosarthur0323@gmail.com>
2022-07-08 07:17:51 -0400

    first commit

---
+++ express.js
@@ -0,0 +1,7 @@
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = "localhost";
+const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
---
+++ index.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
